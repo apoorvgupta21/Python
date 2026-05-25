c32c10f02db02a45e2b485c42714d27d8536c269
apoorvgupta21 <32320383+apoorvgupta21@users.noreply.github.com>
2026-05-25 14:33:25 +0530

    Update ExpenseTracker.py
    Expense Tracker

--- Expense_Tracker/ExpenseTracker.py
+++ Expense_Tracker/ExpenseTracker.py
@@ -1,1 +1,29 @@
-Build a simple expense tracker application to manage your finances. The application should allow users to add, delete, and view their expenses. The application should also provide a summary of the expenses.
+# Create an expense tracker
+# https://roadmap.sh/projects/number-guessing-game
+
+import json
+print("\nWelcome to Expense Tracker! \n")
+
+total_expense = 0
+
+class Expense():
+    def __init__(self, exp_desc, amount):
+        self.exp_desc = exp_desc
+        self.amount = amount
+        #return self.exp_desc, self.amount
+    
+    def vals(self):    
+        #return self.exp_desc, self.amount
+         return {self.exp_desc: self.amount}   
+        
+
+exp1 = Expense("Shoes", "Rs 5000")
+exp2 = Expense("Grocery", "Rs 700")
+exp3 = Expense("OTT", "Rs 4000")
+
+print(exp1.vals())
+print(exp2.vals())  
+
+with open("data.json", "w") as outfile:
+    json.dump(exp1.vals(), outfile, indent=4)
+    json.dump(exp2.vals(), outfile, indent=4)
